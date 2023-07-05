9da1d7a8b5a2ca7093dda5c222d2a49663169e0b
husan <abdigafurovhusan@gmail.com>
2023-07-06 03:52:13 +0500

    fs create and copy done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { resolve } from 'path';
+import { existsSync, mkdirSync, copyFileSync, readdir } from 'fs';
+import customDirname from "../utils/dirname.js";
 const copy = async () => {
     // Write your code here 
+    const newFolderName = resolve(customDirname(import.meta.url), "files_copy");
+    const folderPath = resolve(customDirname(import.meta.url), "files");
+
+    if (!existsSync(folderPath)) {
+        throw Error("FS operation failed");
+    }
+    if (existsSync(newFolderName)) {
+        throw Error("FS operation failed");
+    }
+    mkdirSync(newFolderName);
+
+    readdir(folderPath, (err, files) => {
+        if (err) {
+            console.log(err);
+        } else {
+            files.forEach(file => copyFileSync(resolve(folderPath, file), resolve(newFolderName, file)));
+        }
+    });
 };
 
 await copy();
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,31 @@
+import { dirname, resolve } from "path";
+import { existsSync, writeFile } from "fs";
+
+import { fileURLToPath } from 'url';
+import customDirname from "../utils/dirname.js";
+
+
 const create = async () => {
     // Write your code here 
+    const fileName = resolve(customDirname(import.meta.url), "./files", "fresh.txt");
+    let text = "I am fresh and young";
+
+
+    if (!existsSync(fileName)) {
+        writeFile(fileName, text, (err) => {
+            if (err) {
+                console.log(err);
+            }
+        });
+        return;
+    } else {
+        throw Error("FS operation failed");
+    }
+
+
+    // if (open()) {
+    //     fs;
+    // }
 };
 
 await create();
